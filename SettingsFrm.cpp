86006536d9845ff13f9728ff2dab7300c926c9ad
Krzysztof Grochocki <beherit@users.noreply.github.com>
2013-07-24 14:20:08 +0200

    * Drobne zmiany w sk?rkowaniu formy ustawie?. * Aktualizacja komponent?w AlphaControls do najnowszej dost?pnej wersji (8.19).

--- SettingsFrm.cpp
+++ SettingsFrm.cpp
@@ -10,14 +10,20 @@
 #pragma link "sCheckBox"
 #pragma link "sComboBox"
 #pragma link "sSkinManager"
-#pragma link "sSkinProvider"
+#pragma link "acPNG"
+#pragma link "sBevel"
+#pragma link "sButton"
+#pragma link "sCheckBox"
+#pragma link "sComboBox"
+#pragma link "sSkinManager"
 #pragma resource "*.dfm"
 TSettingsForm *SettingsForm;
 //---------------------------------------------------------------------------
 __declspec(dllimport)UnicodeString GetPluginUserDir();
 __declspec(dllimport)UnicodeString GetThemeSkinDir();
 __declspec(dllimport)bool ChkSkinEnabled();
-__declspec(dllimport)bool ChkNativeEnabled();
+__declspec(dllimport)bool ChkThemeAnimateWindows();
+__declspec(dllimport)bool ChkThemeGlowing();
 __declspec(dllimport)void LoadSettings();
 //---------------------------------------------------------------------------
 
@@ -29,40 +35,31 @@ __fastcall TSettingsForm::TSettingsForm(TComponent* Owner)
 
 void __fastcall TSettingsForm::FormCreate(TObject *Sender)
 {
+  //Wlaczona zaawansowana stylizacja okien
   if(ChkSkinEnabled())
   {
 	UnicodeString ThemeSkinDir = GetThemeSkinDir();
-	if((FileExists(ThemeSkinDir + "\\\\Skin.asz"))&&(!ChkNativeEnabled()))
+	//Plik zaawansowanej stylizacji okien istnieje
+	if(FileExists(ThemeSkinDir + "\\\\Skin.asz"))
 	{
 	  ThemeSkinDir = StringReplace(ThemeSkinDir, "\\\\", "\\", TReplaceFlags() << rfReplaceAll);
 	  sSkinManager->SkinDirectory = ThemeSkinDir;
 	  sSkinManager->SkinName = "Skin.asz";
-	  sSkinProvider->DrawNonClientArea = true;
+	  if(ChkThemeAnimateWindows()) sSkinManager->AnimEffects->FormShow->Time = 200;
+	  else sSkinManager->AnimEffects->FormShow->Time = 0;
+	  sSkinManager->Effects->AllowGlowing = ChkThemeGlowing();
 	  sSkinManager->Active = true;
 	}
-	else
-	 sSkinManager->Active = false;
+	//Brak pliku zaawansowanej stylizacji okien
+	else sSkinManager->Active = false;
   }
+  //Zaawansowana stylizacja okien wylaczona
+  else sSkinManager->Active = false;
 }
 //---------------------------------------------------------------------------
 
 void __fastcall TSettingsForm::FormShow(TObject *Sender)
 {
-  //Skorkowanie okna
-  if(!ChkSkinEnabled())
-  {
-	UnicodeString ThemeSkinDir = GetThemeSkinDir();
-	if((FileExists(ThemeSkinDir + "\\\\Skin.asz"))&&(!ChkNativeEnabled()))
-	{
-	  ThemeSkinDir = StringReplace(ThemeSkinDir, "\\\\", "\\", TReplaceFlags() << rfReplaceAll);
-	  sSkinManager->SkinDirectory = ThemeSkinDir;
-	  sSkinManager->SkinName = "Skin.asz";
-	  sSkinProvider->DrawNonClientArea = false;
-	  sSkinManager->Active = true;
-	}
-	else
-	 sSkinManager->Active = false;
-  }
   //Odczyt ustawien
   TIniFile *Ini = new TIniFile(GetPluginUserDir()+"\\\\BirthdayReminder\\\\Settings.ini");
   //Powiadomienie w dniu urodzin
@@ -99,4 +96,3 @@ void __fastcall TSettingsForm::OKButtonClick(TObject *Sender)
   Close();
 }
 //---------------------------------------------------------------------------
-
--- SettingsFrm.h
+++ SettingsFrm.h
@@ -1,26 +1,23 @@
 //---------------------------------------------------------------------------
-
 #ifndef SettingsFrmH
 #define SettingsFrmH
 //---------------------------------------------------------------------------
-#include <System.Classes.hpp>
-#include <Vcl.Controls.hpp>
-#include <Vcl.StdCtrls.hpp>
-#include <Vcl.Forms.hpp>
 #include "acPNG.hpp"
 #include "sBevel.hpp"
 #include "sButton.hpp"
 #include "sCheckBox.hpp"
 #include "sComboBox.hpp"
-#include <Vcl.ExtCtrls.hpp>
-#include <System.Actions.hpp>
-#include <Vcl.ActnList.hpp>
 #include "sSkinManager.hpp"
-#include "sSkinProvider.hpp"
 #include <IdBaseComponent.hpp>
 #include <IdCoder.hpp>
 #include <IdCoder3to4.hpp>
 #include <IdCoderMIME.hpp>
+#include <System.Actions.hpp>
+#include <System.Classes.hpp>
+#include <Vcl.ActnList.hpp>
+#include <Vcl.Controls.hpp>
+#include <Vcl.ExtCtrls.hpp>
+#include <Vcl.StdCtrls.hpp>
 //---------------------------------------------------------------------------
 class TSettingsForm : public TForm
 {
@@ -33,14 +30,12 @@ __published:	// IDE-managed Components
 	TsComboBox *AnotherDayComboBox;
 	TActionList *ActionList;
 	TAction *aExit;
-	TsSkinProvider *sSkinProvider;
 	TsSkinManager *sSkinManager;
 	TIdDecoderMIME *IdDecoderMIME;
 	void __fastcall aExitExecute(TObject *Sender);
 	void __fastcall OKButtonClick(TObject *Sender);
 	void __fastcall FormCreate(TObject *Sender);
 	void __fastcall FormShow(TObject *Sender);
-
 private:	// User declarations
 public:		// User declarations
 	bool AddSource;
