32a4c1030cde73b394ebdc72ca47c0225aab2ae4
Krzysztof Grochocki <beherit@users.noreply.github.com>
2013-07-24 14:18:31 +0200

    * Wtyczka zosta?a napisana ca?kowicie od nowa. * Przeniesienie mechanizmu wtyczki do centrum powiadomie?. * Pe?ne sk?rkowanie okna wtyczki. * Nowa ikonka skojarzona ze wtyczk?. * Wtyczka posiada w?asn? ikon? pokazywan? w ustawieniach komunikatora na li?cie wtyczek.

--- SettingsFrm.cpp
+++ SettingsFrm.cpp
@@ -1,320 +1,101 @@
 //---------------------------------------------------------------------------
 #include <vcl.h>
 #pragma hdrstop
-#include <inifiles.hpp>
-#include <time.h>
 #include "SettingsFrm.h"
 //---------------------------------------------------------------------------
 #pragma package(smart_init)
-#pragma link "XPMan"
-#pragma link "IdCoderMIME"
-#pragma link "IdAntiFreeze"
-#pragma link "IdAntiFreezeBase"
+#pragma link "acPNG"
+#pragma link "sBevel"
+#pragma link "sButton"
+#pragma link "sCheckBox"
+#pragma link "sComboBox"
+#pragma link "sSkinManager"
+#pragma link "sSkinProvider"
 #pragma resource "*.dfm"
 TSettingsForm *SettingsForm;
 //---------------------------------------------------------------------------
-__declspec(dllimport)void TestChmurki(int TimeOutTest,bool ShowAgeTest, bool PlaySoundTest);
-__declspec(dllimport)void ShowBirthdayInfo(UnicodeString CText, int CTimeOut, bool CSoundPlay);
-__declspec(dllimport)UnicodeString GetPluginPath();
-__declspec(dllimport)UnicodeString GetContactsPath();
-__declspec(dllimport)UnicodeString GetContactNick(UnicodeString JID);
-__declspec(dllimport)bool AQQSoundOff();
-//---------------------------------------------------------------------------
-UnicodeString ePluginDirectory;
-UnicodeString eContactsPath;
+__declspec(dllimport)UnicodeString GetPluginUserDir();
+__declspec(dllimport)UnicodeString GetThemeSkinDir();
+__declspec(dllimport)bool ChkSkinEnabled();
+__declspec(dllimport)bool ChkNativeEnabled();
+__declspec(dllimport)void LoadSettings();
 //---------------------------------------------------------------------------
 
 __fastcall TSettingsForm::TSettingsForm(TComponent* Owner)
-        : TForm(Owner)
+	: TForm(Owner)
 {
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TSettingsForm::FormCreate(TObject *Sender)
+{
+  if(ChkSkinEnabled())
+  {
+	UnicodeString ThemeSkinDir = GetThemeSkinDir();
+	if((FileExists(ThemeSkinDir + "\\\\Skin.asz"))&&(!ChkNativeEnabled()))
+	{
+	  ThemeSkinDir = StringReplace(ThemeSkinDir, "\\\\", "\\", TReplaceFlags() << rfReplaceAll);
+	  sSkinManager->SkinDirectory = ThemeSkinDir;
+	  sSkinManager->SkinName = "Skin.asz";
+	  sSkinProvider->DrawNonClientArea = true;
+	  sSkinManager->Active = true;
+	}
+	else
+	 sSkinManager->Active = false;
+  }
 }
 //---------------------------------------------------------------------------
 
 void __fastcall TSettingsForm::FormShow(TObject *Sender)
 {
-  aReadSettings->Execute();
-}
-//---------------------------------------------------------------------------
-
-void __fastcall TSettingsForm::OkButtonClick(TObject *Sender)
-{
-  aSaveSettings->Execute();
-  Close();
-}
-//---------------------------------------------------------------------------
-
-void __fastcall TSettingsForm::TestButonClick(TObject *Sender)
-{
-  TestChmurki(TimeBox->ItemIndex + 3, AgeCheckBox->Checked, SoundCheckBox->Checked);
-}
-//---------------------------------------------------------------------------
-
-void __fastcall TSettingsForm::TimerTimer(TObject *Sender)
-{
-  ePluginDirectory=GetPluginPath();
-
-  TIniFile *Ini = new TIniFile(ePluginDirectory + "\\\\BirthdayReminder\\\\Settings.ini");
-  int Repeat = Ini->ReadInteger("Settings", "Repeat", 0);
-  delete Ini;
-
-  if (Repeat==1) //Raz dziennie
+  //Skorkowanie okna
+  if(!ChkSkinEnabled())
   {
-    TDateTime tmpTodey = TDateTime::CurrentDate();
-    UnicodeString tmpCurrentDate = tmpTodey;
-
-    TIniFile *Ini = new TIniFile(ePluginDirectory + "\\\\BirthdayReminder\\\\Settings.ini");
-    UnicodeString tmpCurrentDay = Ini->ReadString("Settings", "CurrentDay", "");
-    delete Ini;
-
-    if(AnsiSameStr(tmpCurrentDay,tmpCurrentDate))
-     Timer->Enabled=false;
-    else
-    {
-      aFindContacts->Execute();
-
-      TIniFile *Ini = new TIniFile(ePluginDirectory + "\\\\BirthdayReminder\\\\Settings.ini");
-      Ini->WriteString("Settings", "CurrentDay", tmpCurrentDate);
-      delete Ini;
-
-	  Timer->Enabled=false;
+	UnicodeString ThemeSkinDir = GetThemeSkinDir();
+	if((FileExists(ThemeSkinDir + "\\\\Skin.asz"))&&(!ChkNativeEnabled()))
+	{
+	  ThemeSkinDir = StringReplace(ThemeSkinDir, "\\\\", "\\", TReplaceFlags() << rfReplaceAll);
+	  sSkinManager->SkinDirectory = ThemeSkinDir;
+	  sSkinManager->SkinName = "Skin.asz";
+	  sSkinProvider->DrawNonClientArea = false;
+	  sSkinManager->Active = true;
 	}
+	else
+	 sSkinManager->Active = false;
   }
-  else
-  {
-    aFindContacts->Execute();
-
-    if (Repeat==0) //Przy ka¿dym w³¹czeniu AQQ
-     Timer->Enabled=false;
-    else //Inne tam
-     Timer->Interval = (Repeat-1) * 3600000;
-  }
-}
-//---------------------------------------------------------------------------
-
-void __fastcall TSettingsForm::aFindContactsExecute(TObject *Sender)
-{
-  IdAntiFreeze->Active=true;
-  NicksList->Clear();
-  int index; //Do sprawdzania nikow
-
-  ePluginDirectory=GetPluginPath();
-  eContactsPath=GetContactsPath();
-  FileListBox->Clear();
-  FileListBox->Directory=eContactsPath;
-
-  Word tYear=0,tMonth=0,tDay=0; //Do rozkodowanej aktualnej data
-  TDateTime Todey = TDateTime::CurrentDate(); //Pobranie aktualnej daty zakodowanej
-  DecodeDate(Todey, tYear, tMonth, tDay); //Rozkodowanie aktualnej daty
-  Word WbYear=0,WbMonth=0,WbDay=0; //Do rozkodowanej daty urodzin kontaktu
-  UnicodeString AbYear=0,AbMonth=0,AbDay=0; //j.w.
-  TDateTime BirthDay; //Data urodzin kontaktu
-  UnicodeString BirthDayAnsi; //Data urodzin kontaktu
-
-  //do porównania dat
-  double DataAktualna;
-  double DataKontaktu;
-  int RoznicaDat;
-
-  UnicodeString FileName; //Nazwa pliku ini
-  UnicodeString Text; //Tekst na chmurce
-
-  //odczyt ustawien
-  TIniFile *Ini = new TIniFile(ePluginDirectory + "\\\\BirthdayReminder\\\\Settings.ini");
-  int TimeOut = Ini->ReadInteger("Settings", "TimeOut", 6);
-  TimeOut = TimeOut * 1000;
-  int AnotherDay = Ini->ReadInteger("Settings", "Another", 0);
-  bool SoundPlay = Ini->ReadBool("Settings", "Sound", 1);
-  if(SoundPlay==true)
-   SoundPlay=AQQSoundOff();
-  bool InBirthDay = Ini->ReadBool("Settings", "InBirthDay", 1);
-  bool ShowAge = Ini->ReadBool("Settings", "ShowAge", 1);
-  delete Ini;
-
-  for(int i=0;i<FileListBox->Items->Count;i++)
-  {
-    FileListBox->ItemIndex=i;
-
-    FileName = FileListBox->FileName;
-    FileName = StringReplace(FileName, "\\", "\\\\", TReplaceFlags() << rfReplaceAll);
-
-	TIniFile *Ini = new TIniFile(FileName);
-	BirthDayAnsi = IdDecoderMIME->DecodeString((Ini->ReadString("Buddy", "Birth", "A===").c_str()));
-    delete Ini;
-
-    try
-    {
-	  if(AnsiPos("-",BirthDayAnsi)>0)
-	  {
-        //Wyciagniecie roku urodzin
-		AbYear = BirthDayAnsi;
-		AbYear = AbYear.Delete(AnsiPos("-",AbYear),AbYear.Length());
-
-        //Wyciagniecie miesiaca urodzin
-		AbMonth = BirthDayAnsi;
-        AbMonth = AbMonth.Delete(1,AnsiPos("-",AbMonth));
-        AbMonth = AbMonth.Delete(AnsiPos("-",AbMonth),AbMonth.Length());
-
-        //Wyciagniecie dnia urodzin
-		AbDay = BirthDayAnsi;
-        AbDay = AbDay.Delete(1,AnsiPos("-",AbDay));
-		AbDay = AbDay.Delete(1,AnsiPos("-",AbDay));
-
-        //Zakodowanie do domyslnego formatu i odkodowanie
-		BirthDay = EncodeDate(StrToInt(AbYear),StrToInt(AbMonth),StrToInt(AbDay));
-		DecodeDate(BirthDay,WbYear,WbMonth,WbDay);
-
-        //Porownanie dat
-        DataAktualna = EncodeDate(tYear, tMonth, tDay);
-        DataKontaktu = EncodeDate(tYear, WbMonth, WbDay);
-        RoznicaDat = difftime(DataKontaktu, DataAktualna);
-
-		if((InBirthDay==1)&&(RoznicaDat==0))
-        {
-		  UnicodeString Nick = GetContactNick(ExtractFileName(FileListBox->FileName).SetLength(ExtractFileName(FileListBox->FileName).Length()-4));
-
-		  int index = NicksList->Items->IndexOf(Nick);
-          if(index==-1)
-          {
-            WbYear = tYear - WbYear;
-
-			Text = Nick + " obchodzi dziœ urodziny!";
-            if(ShowAge==1)
-			 Text = Text + " (" + WbYear + ")";
-
-			ShowBirthdayInfo(Text, TimeOut, SoundPlay);
-
-			SoundPlay=0;
-            NicksList->Items->Add(Nick);
-          }
-        }
-
-        if((RoznicaDat==1)||(RoznicaDat==2)||(RoznicaDat==3)||(RoznicaDat==4)||(RoznicaDat==5)||(RoznicaDat==6)||(RoznicaDat==7)||(RoznicaDat==8))
-        {
-          if(AnotherDay!=0)
-          {
-            UnicodeString Nick = GetContactNick(ExtractFileName(FileListBox->FileName).SetLength(ExtractFileName(FileListBox->FileName).Length()-4));
-
-			int index = NicksList->Items->IndexOf(Nick);
-            if(index==-1)
-            {
-              WbYear = tYear - WbYear;
-
-              if(AnotherDay==1)
-              {
-				Text = Nick + " obchodzi jutro urodziny!";
-                if(ShowAge==1)
-                 Text = Text + " (" + WbYear + ")";
-			  }
-              if(AnotherDay==2)
-              {
-				Text = Nick + " za dwa dni obchodzi urodziny!";
-				if(ShowAge==1)
-				 Text = Text + " (" + WbYear + ")";
-			  }
-              if(AnotherDay==3)
-              {
-				Text = Nick + " za trzy dni obchodzi urodziny!";
-                if(ShowAge==1)
-                 Text = Text + " (" + WbYear + ")";
-			  }
-              if(AnotherDay==4)
-              {
-                Text = Nick + " za cztery dni obchodzi urodziny!";
-                if(ShowAge==1)
-                 Text = Text + " (" + WbYear + ")";
-			  }
-              if(AnotherDay==5)
-              {
-                Text = Nick + " za piêæ dni obchodzi urodziny!";
-                if(ShowAge==1)
-                 Text = Text + " (" + WbYear + ")";
-			  }
-              if(AnotherDay==6)
-              {
-                Text = Nick + " za szeœæ dni obchodzi urodziny!";
-                if(ShowAge==1)
-                 Text = Text + " (" + WbYear + ")";
-			  }
-              if(AnotherDay==7)
-              {
-				Text = Nick + " za tydzieñ obchodzi urodziny!";
-				if(ShowAge==1)
-                 Text = Text + " (" + WbYear + ")";
-			  }
-              if(AnotherDay==8)
-              {
-                Text = Nick + " za dwa tygodnie obchodzi urodziny!";
-                if(ShowAge==1)
-                 Text = Text + " (" + WbYear + ")";
-			  }
-
-			  if(((RoznicaDat==1)&&(AnotherDay==1))||
-			  ((RoznicaDat==2)&&(AnotherDay==2))||
-			  ((RoznicaDat==3)&&(AnotherDay==3))||
-			  ((RoznicaDat==4)&&(AnotherDay==4))||
-			  ((RoznicaDat==5)&&(AnotherDay==5))||
-			  ((RoznicaDat==6)&&(AnotherDay==6))||
-			  ((RoznicaDat==7)&&(AnotherDay==7))||
-			  ((RoznicaDat==14)&&(AnotherDay==8)))
-			  {
-				ShowBirthdayInfo(Text, TimeOut, SoundPlay);
-				SoundPlay=0;
-				NicksList->Items->Add(Nick);
-			  }
-			}
-		  }
-		}
-	  }
-	}
-	catch (...) { /*b³¹d - nie rób nic*/ }
-  }
-  IdAntiFreeze->Active=false;
-}
-//---------------------------------------------------------------------------
-
-void __fastcall TSettingsForm::aSaveSettingsExecute(TObject *Sender)
-{
-  ePluginDirectory=GetPluginPath();
-
-  TIniFile *Ini = new TIniFile(ePluginDirectory + "\\\\BirthdayReminder\\\\Settings.ini");
-
-  Ini->WriteInteger("Settings", "TimeOut", TimeBox->ItemIndex + 3);
-
-  Ini->WriteBool("Settings", "Sound", SoundCheckBox->Checked);
-  Ini->WriteBool("Settings", "InBirthDay", BirthDayCheckBox->Checked);
-  Ini->WriteBool("Settings", "ShowAge", AgeCheckBox->Checked);
-
-  Ini->WriteInteger("Settings", "Another", AnotherDayBox->ItemIndex);
-
-  Ini->WriteInteger("Settings", "Repeat", RepeatCheckBox->ItemIndex);
-
-  delete Ini;        
-}
-//---------------------------------------------------------------------------
-
-void __fastcall TSettingsForm::aReadSettingsExecute(TObject *Sender)
-{
-  ePluginDirectory=GetPluginPath();
-
-  TIniFile *Ini = new TIniFile(ePluginDirectory + "\\\\BirthdayReminder\\\\Settings.ini");
-
-  int eTimeOut = Ini->ReadInteger("Settings", "TimeOut", 6);
-  TimeBox->ItemIndex=eTimeOut - 3;
-
-  SoundCheckBox->Checked=Ini->ReadBool("Settings", "Sound", 1);
-  BirthDayCheckBox->Checked=Ini->ReadBool("Settings", "InBirthDay", 1);
-  AgeCheckBox->Checked=Ini->ReadBool("Settings", "ShowAge", 1);
-
-  AnotherDayBox->ItemIndex = Ini->ReadInteger("Settings", "Another", 0);
-
-  RepeatCheckBox->ItemIndex = Ini->ReadInteger("Settings", "Repeat", 0);
-
+  //Odczyt ustawien
+  TIniFile *Ini = new TIniFile(GetPluginUserDir()+"\\\\BirthdayReminder\\\\Settings.ini");
+  //Powiadomienie w dniu urodzin
+  InBirthDayCheckBox->Checked = Ini->ReadBool("Settings","InBirthDay",true);
+  //Informowanie rowniez w innym okresie
+  AnotherDayComboBox->ItemIndex = Ini->ReadInteger("Settings","Another",0);
   delete Ini;
 }
 //---------------------------------------------------------------------------
 
 void __fastcall TSettingsForm::aExitExecute(TObject *Sender)
 {
+  //Odzaczenie typu zamkniecia formy
+  AddSource = false;
+  //Zamkniecie formy
+  Close();
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TSettingsForm::OKButtonClick(TObject *Sender)
+{
+  //Zapisanie nowych ustawien
+  TIniFile *Ini = new TIniFile(GetPluginUserDir()+"\\\\BirthdayReminder\\\\Settings.ini");
+  //Powiadomienie w dniu urodzin
+  Ini->WriteBool("Settings","InBirthDay",InBirthDayCheckBox->Checked);
+  //Informowanie rowniez w innym okresie
+  Ini->WriteInteger("Settings","Another",AnotherDayComboBox->ItemIndex);
+  delete Ini;
+  //Odczyt ustawien w rdzeniu wtyczki
+  LoadSettings();
+  //Odzaczenie typu zamkniecia formy
+  AddSource = true;
+  //Zamkniecie formy
   Close();
 }
 //---------------------------------------------------------------------------
--- SettingsFrm.h
+++ SettingsFrm.h
@@ -3,59 +3,48 @@
 #ifndef SettingsFrmH
 #define SettingsFrmH
 //---------------------------------------------------------------------------
-#include <Classes.hpp>
-#include <Controls.hpp>
-#include <StdCtrls.hpp>
-#include <Forms.hpp>
-#include <ExtCtrls.hpp>
-#include "XPMan.hpp"
-#include "IdCoderMIME.hpp"
+#include <System.Classes.hpp>
+#include <Vcl.Controls.hpp>
+#include <Vcl.StdCtrls.hpp>
+#include <Vcl.Forms.hpp>
+#include "acPNG.hpp"
+#include "sBevel.hpp"
+#include "sButton.hpp"
+#include "sCheckBox.hpp"
+#include "sComboBox.hpp"
+#include <Vcl.ExtCtrls.hpp>
+#include <System.Actions.hpp>
+#include <Vcl.ActnList.hpp>
+#include "sSkinManager.hpp"
+#include "sSkinProvider.hpp"
 #include <IdBaseComponent.hpp>
 #include <IdCoder.hpp>
 #include <IdCoder3to4.hpp>
-#include <FileCtrl.hpp>
-#include <ActnList.hpp>
-#include <ComCtrls.hpp>
-#include "IdAntiFreeze.hpp"
-#include "IdAntiFreezeBase.hpp"
+#include <IdCoderMIME.hpp>
 //---------------------------------------------------------------------------
 class TSettingsForm : public TForm
 {
 __published:	// IDE-managed Components
-	TLabel *TimeNotLabel;
-        TButton *OkButton;
-        TComboBox *TimeBox;
-        TCheckBox *SoundCheckBox;
-        TCheckBox *BirthDayCheckBox;
-        TComboBox *AnotherDayBox;
-        TLabel *Another;
-        TCheckBox *AgeCheckBox;
-        TButton *TestButon;
-	TTimer *Timer;
-        TLabel *RepeatLabel;
-        TComboBox *RepeatCheckBox;
-        TBevel *Bevel;
-        TIdDecoderMIME *IdDecoderMIME;
-        TFileListBox *FileListBox;
-        TActionList *ActionList;
-        TAction *aFindContacts;
-        TListBox *NicksList;
-        TAction *aSaveSettings;
-        TAction *aReadSettings;
-	TTabControl *TabControl;
+	TsBevel *Bevel;
+	TsButton *CancelButton;
+	TsButton *OKButton;
+	TImage *GitfImage;
+	TsCheckBox *InBirthDayCheckBox;
+	TsComboBox *AnotherDayComboBox;
+	TActionList *ActionList;
 	TAction *aExit;
-	TIdAntiFreeze *IdAntiFreeze;
-        void __fastcall FormShow(TObject *Sender);
-        void __fastcall OkButtonClick(TObject *Sender);
-        void __fastcall TestButonClick(TObject *Sender);
-        void __fastcall TimerTimer(TObject *Sender);
-        void __fastcall aFindContactsExecute(TObject *Sender);
-        void __fastcall aSaveSettingsExecute(TObject *Sender);
-        void __fastcall aReadSettingsExecute(TObject *Sender);
+	TsSkinProvider *sSkinProvider;
+	TsSkinManager *sSkinManager;
+	TIdDecoderMIME *IdDecoderMIME;
 	void __fastcall aExitExecute(TObject *Sender);
+	void __fastcall OKButtonClick(TObject *Sender);
+	void __fastcall FormCreate(TObject *Sender);
+	void __fastcall FormShow(TObject *Sender);
+
 private:	// User declarations
 public:		// User declarations
-        __fastcall TSettingsForm(TComponent* Owner);
+	bool AddSource;
+	__fastcall TSettingsForm(TComponent* Owner);
 };
 //---------------------------------------------------------------------------
 extern PACKAGE TSettingsForm *SettingsForm;
