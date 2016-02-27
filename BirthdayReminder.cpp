80df20ed98f000ad480d58021868b679826c4060
Krzysztof Grochocki <beherit@users.noreply.github.com>
2016-02-27 12:47:53 +0100

    Zmiana adresu email i URL strony domowej + aktualizacja copyright i sposobu kontaktu

--- BirthdayReminder.cpp
+++ BirthdayReminder.cpp
@@ -1,5 +1,5 @@
 //---------------------------------------------------------------------------
-// Copyright (C) 2009-2015 Krzysztof Grochocki
+// Copyright (C) 2009-2016 Krzysztof Grochocki
 //
 // This file is part of Birthday Reminder
 //
@@ -912,9 +912,9 @@ extern "C" PPluginInfo __declspec(dllexport) __stdcall AQQPluginInfo(DWORD AQQVe
 	PluginInfo.Version = PLUGIN_MAKE_VERSION(3,3,0,0);
 	PluginInfo.Description = L"Powiadamia, poprzez centrum powiadomieñ, o urodzinach kontaktów z naszej listy.";
 	PluginInfo.Author = L"Krzysztof Grochocki";
-	PluginInfo.AuthorMail = L"kontakt@beherit.pl";
+	PluginInfo.AuthorMail = L"contact@beherit.pl";
 	PluginInfo.Copyright = L"Krzysztof Grochocki";
-	PluginInfo.Homepage = L"http://beherit.pl";
+	PluginInfo.Homepage = L"beherit.pl";
 	PluginInfo.Flag = 0;
 	PluginInfo.ReplaceDefaultModule = 0;
 
